8b97894cc9999d3a987b6a8812940c041594f240
Philopater06 <philoessam2016@gmail.com>
2026-05-06 00:34:53 +0300

    add file system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,6 +10,7 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init(&filesys_lock);
 }
 
 static void
@@ -17,4 +18,68 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+
+bool file_create (const char *file, unsigned initial_size){
+  lock_acquire(&filesys_lock);
+  bool result = filesys_create(file, initial_size);
+  lock_release(&filesys_lock);
+  return result;
+}
+
+bool file_remove (const char *file){
+  lock_acquire(&filesys_lock);
+  bool result = filesys_remove(file);
+  lock_release(&filesys_lock);
+  return result;
+}
+
+struct file *file_open (const char *file){
+  lock_acquire(&filesys_lock);
+  struct file *result = filesys_open(file);
+  lock_release(&filesys_lock);
+  return result;
+}
+
+int file_filesize (struct file *fd){
+  lock_acquire(&filesys_lock);
+  int result = file_length(fd);
+  lock_release(&filesys_lock);
+  return result;
+}
+
+int file_fileread (struct file *fd, void *buffer, unsigned size){
+  lock_acquire(&filesys_lock);
+  int result = file_read(fd, buffer, size);
+  lock_release(&filesys_lock);
+  return result;
+}
+
+int file_filewrite (struct file *fd, const void *buffer, unsigned size){
+  lock_acquire(&filesys_lock);
+  deny_write(fd);
+  int res = file_write(fd, buffer, size);
+  allow_write(fd);
+  lock_release(&filesys_lock);
+  return res;
+}
+
+void file_seek (struct file *fd, unsigned position){
+  lock_acquire(&filesys_lock);
+  file_seek(fd, position);
+  lock_release(&filesys_lock);
+}
+
+unsigned file_tell (struct file *fd){
+  lock_acquire(&filesys_lock);
+  unsigned result = file_tell(fd);
+  lock_release(&filesys_lock);
+  return result;
+}
+
+void file_close (struct file *fd){
+  lock_acquire(&filesys_lock);
+  file_close(fd);
+  lock_release(&filesys_lock);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,19 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+
+struct lock filesys_lock;
+
 void syscall_init (void);
+bool file_create (const char *file, unsigned initial_size);
+bool file_remove (const char *file);
+struct file *file_open (const char *file);
+int file_filesize (struct file *fd);   
+int file_read (struct file *fd, void *buffer, unsigned size);
+int file_write (struct file *fd, const void *buffer, unsigned size);
+void file_seek (struct file *fd, unsigned position);
+unsigned file_tell (struct file *fd);
+void file_close (struct file *fd);
 
 #endif /* userprog/syscall.h */
